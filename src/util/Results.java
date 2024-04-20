c3ed700ade8641e163e79194a54f2ca3f83fd44b
Elia <o.elia.batista@gmail.com>
2024-04-20 09:49:58 -0600

    exps

--- src/util/Results.java
+++ src/util/Results.java
@@ -476,7 +476,7 @@ public class Results {
         String localities [] = {"95"};
         short numnodes []    = {3};
         String algos []      = {"flexcast"};// , "flexcast", "skeen"};
-        int clients []       = {3};//{24,240,480,720,960,1200,1440};
+        int clients []       = {120};//{24,240,480,720,960,1200,1440};
         int gcflex           = 100;
         // int gcall            = 0;
         // int dag              = 1;
