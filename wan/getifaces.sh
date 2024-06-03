ee66ae192d0081b63242dfd699feb0689a87356a
Elia <delime@usi.ch>
2024-06-03 11:33:29 +0200

    wan fixes for cluster

--- wan/getifaces.sh
+++ wan/getifaces.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-if [ "$#" -lt 1 ]; then 
-    echo  "Usage: $0 <#nodes>"
+if [ "$#" -lt 5 ]; then 
+    echo  "Usage: $0 <nodesfrom> <nodesto> <netip> <basedir> <iscloudlab>"
     exit 0; 
 fi
 
-basedir=/usr/batista/flexcast;
+basedir=$4;
 rm -f $basedir/wan/ifaces.csv;
-for i in $(seq 1 $(($1)))
+for i in $(seq $(($1)) $(($2)))
 do
-    iface=$(ssh node$i "ip route list 10.10.1.0/24" | awk '{print $3}')
-    echo "node$i,10.10.1."$(($i+1))",$iface" >> $basedir/wan/ifaces.csv;
+    iface=$(ssh node$i "ip route list $3.0/24" | awk '{print $3}')
+    if [ "$5" == "true" ]; then 
+        echo "node$i,$3."$(($i+1))",$iface" >> $basedir/wan/ifaces.csv;
+    else
+        echo "node$i,$3."$(($i))",$iface" >> $basedir/wan/ifaces.csv;
+    fi
     iniport=$(($iniport+10));
 done
---
+++ wan/unset_original.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+BASEDIR=$(dirname $(readlink -f $0))
+echo "Base directory: $BASEDIR"
+
+# Format: "$ZONE,$IP"
+IPS_FILE=$BASEDIR/ips.csv
+if [ ! -r "$IPS_FILE" ]
+then
+	echo "ERROR: unable to find IPs file '$IPS_FILE'"
+	exit 1
+fi
+echo "IPs file: $IPS_FILE"
+
+IFACES_FILE=$BASEDIR/ifaces.csv
+echo "Ifaces file: $IFACES_FILE"
+touch $IFACES_FILE
+
+for IP in $(cut -d, -f2 $IPS_FILE)
+do
+	[ -z "$IP" ] && continue
+	[ "${IP#IP}" != "$IP" ] && continue
+
+	ZONE=$(grep "$IP$" $IPS_FILE | cut -d, -f1)
+	[ -z "$ZONE" ] && continue
+
+	UPDATE_IFACES_FILE=
+	NODE=$(grep ",$IP," $IFACES_FILE | cut -d, -f1)
+	IFACE=$(grep ",$IP," $IFACES_FILE | cut -d, -f3)
+	if [ -z "$NODE" -o -z "$IFACE" ]
+	then
+		NODE=$(dig +short -x $IP | head -n1)
+		NODE=${NODE%.}
+
+		echo -n "Retrieving IFACE for $NODE: "
+		IFACE=$(ssh $NODE ip -br addr show to 192.168.3.0/24)
+		IFACE=$(echo $IFACE | cut -d' ' -f1)
+		echo "$IFACE"
+
+		UPDATE_IFACES_FILE=yes
+	fi
+
+	if [ -z "$NODE" -o -z "$IFACE" ]
+	then
+		echo "ERROR: unable to find NODE or IFACE for $IP"
+		exit 3
+	fi
+
+	echo "- $NODE ($IP $IFACE) $ZONE"
+
+	if [ -n "$UPDATE_IFACES_FILE" ]
+	then
+		echo "$NODE,$IP,$IFACE" >> $IFACES_FILE
+	fi
+
+	COMMAND="ssh $NODE sudo latency-setter unset $IFACE"
+	echo $ $COMMAND
+	$COMMAND
+	if [ "$?" != "0" ]
+	then
+		echo "ERROR: to unset node $NODE ($IP)"
+		exit 4
+	fi
+done
+
+echo "Done."
